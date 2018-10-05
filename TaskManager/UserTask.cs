89c18d4c414b5833bd7de8759f66e08db0c92d34
GKenar <kenar.gf@gmail.com>
2018-10-05 15:48:03 +0300

    Добавлены классы UserTask и DBUserTask представляющие задачу #TM-5

---
+++ TaskManager/UserTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public enum TaskPriority { Low, Medium, High } //Undef?
+
+    public class UserTask
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TaskPriority Priority { get; set; }
+        public DateTime TaskDate { get; set; }
+        public DateTime NotifyDate { get; set; }
+    }
+}
