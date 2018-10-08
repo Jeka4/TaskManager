dcf41f09d6e6652de815b0754ec4445e35fa3ac9
GKenar <kenar.gf@gmail.com>
2018-10-08 21:02:24 +0300

    Добавлены тестовые методы GetAllTasks и GetTasksOfDay

--- TaskManager/DataModels/DataModel.cs
+++ TaskManager/DataModels/DataModel.cs
@@ -4,11 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LinqToDB;
+using DataModels;
 
 namespace TaskManager
 {
     public class DataModel : IDataModel
     {
-         
+        public List<UserTask> GetAllTasks()
+        {
+            List<UserTask> tasks = new List<UserTask>();
+            using (var db = new UserTasksDB())
+            {
+                var query = from t in db.UserTasks
+                            from d in db.TaskDates.Where(q => q.Id == t.TaskDateID).DefaultIfEmpty() //?????
+                            from n in db.NotifyDates.Where(q => q.Id == t.NotifyDateID).DefaultIfEmpty()
+                            select new UserTask //Можно расширить класс, добавив конструктор с TaskDate, NotifyDate
+                            {
+                                Name = t.Name,
+                                Description = t.Description,
+                                Priority = t.Priority,
+                                TaskDateID = t.TaskDateID,
+                                TaskDate = d,
+                                NotifyDateID = t.NotifyDateID,
+                                NotifyDate = n
+                            };
+
+                tasks = query.ToList();
+            }
+            return tasks;
+        }
+
+        public List<UserTask> GetTasksOfDay(DateTime date) //Использовать DateTime? + дублирование кода
+        {
+            List<UserTask> tasks = new List<UserTask>();
+            using (var db = new UserTasksDB())
+            {
+                var query = from t in db.UserTasks
+                            from d in db.TaskDates.Where(q => q.Id == t.TaskDateID).DefaultIfEmpty() //?????
+                            from n in db.NotifyDates.Where(q => q.Id == t.NotifyDateID).DefaultIfEmpty()
+                            where d.Date == date.ToShortDateString()
+                            select new UserTask
+                            {
+                                Name = t.Name,
+                                Description = t.Description,
+                                Priority = t.Priority,
+                                TaskDateID = t.TaskDateID,
+                                TaskDate = d,
+                                NotifyDateID = t.NotifyDateID,
+                                NotifyDate = n
+                            };
+
+                tasks = query.ToList();
+            }
+            return tasks;
+        }
     }
 }
--- TaskManager/DataModels/IDataModel.cs
+++ TaskManager/DataModels/IDataModel.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataModels;
 
 namespace TaskManager
 {
     public interface IDataModel
     {
-
+        List<UserTask> GetAllTasks();
+        List<UserTask> GetTasksOfDay(DateTime date);
     }
 }
