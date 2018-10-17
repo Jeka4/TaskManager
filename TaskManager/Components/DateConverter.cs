5c303f5800058c33341df7f31f903e326c386483
GKenar <kenar.gf@gmail.com>
2018-10-17 23:15:13 +0300

    Добавлен класс DateConverter

---
+++ TaskManager/Components/DateConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Components
+{
+    public class DateConverter : IDateConverter
+    {
+        private readonly string _dataFormat;
+
+        /// <summary>
+        /// Конструктор DateConverter
+        /// </summary>
+        /// <param name="dataFormat">Шаблон даты</param>
+        public DateConverter(string dataFormat)
+        {
+            if (_dataFormat == string.Empty)
+                throw new ArgumentException("Format string should not be empty.");
+
+            _dataFormat = dataFormat;
+        }
+
+        /// <summary>
+        /// Преобразует строку в DateTime согласно заданному формату
+        /// </summary>
+        /// <param name="date">Дата</param>
+        /// <returns></returns>
+        public DateTime ParseDateToString(string date)
+        {
+            if (date == string.Empty)
+                throw new ArgumentException("Date string should not be empty.");
+
+            return DateTime.ParseExact(date, _dataFormat, System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Преобразует дату в строку согласно заданному формату
+        /// </summary>
+        /// <param name="date">Дата</param>
+        /// <returns></returns>
+        public string ConvertDateToString(DateTime date)
+        {
+            return date.ToString(_dataFormat);
+        }
+    }
+}
