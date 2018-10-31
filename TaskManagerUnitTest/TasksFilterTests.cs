f80776a4a6b3e1f959b40f86da7aa3789993c988
GKenar <kenar.gf@gmail.com>
2018-10-31 23:00:08 +0200

    Добавлен проект UnitTest. Начата разработка теста к TasksFilter #TM-20

---
+++ TaskManagerUnitTest/TasksFilterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using TaskManager.DataModelComponents;
+using System.Collections.Generic;
+using TaskManager.DataModels;
+using TaskManager.Components;
+using System.Linq;
+
+namespace TaskManagerUnitTest
+{
+    [TestFixture]
+    public class TasksFilterTests
+    {
+        private List<UserTask> GenerateUserTaskList()
+        {
+            return new List<UserTask>
+            {
+                new UserTask { Priority = TaskPriority.High.ToString() },
+                new UserTask { Priority = TaskPriority.Medium.ToString() },
+                new UserTask { Priority = TaskPriority.Low.ToString() }
+            };
+        }
+
+        [Test]
+        public void FilterHighPriority()
+        {
+            var filter = new TasksFilter();
+            var usertasks = GenerateUserTaskList();
+
+            var result = filter.Filter(usertasks.AsQueryable(), FilterType.HighPriority);
+
+            Assert.IsTrue(result.All(t => t.Priority == TaskPriority.High.ToString()));
+        }
+    }
+}
