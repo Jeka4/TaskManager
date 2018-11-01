d7b10101cb95ce28656fab79f74e56fd481ad75d
Пользователь <mecoji@youzend.net>
2018-11-01 08:12:58 +0200

    Реализован тест для TasksFilter. Начата разработка теста для DateConverter

---
+++ TaskManagerUnitTest/DateConverterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using TaskManager.PresenterComponents;
+
+namespace TaskManagerUnitTest
+{
+    [TestFixture]
+    public class DateConverterTests
+    {
+        [Test]
+        public void FormatStringIsNull()
+        {
+            string formatString = null;
+
+            Assert.Throws<ArgumentException>(() =>
+                new DateConverter(formatString)
+            );
+        }
+    }
+}
--- TaskManagerUnitTest/TasksFilterTests.cs
+++ TaskManagerUnitTest/TasksFilterTests.cs
@@ -17,19 +17,55 @@ namespace TaskManagerUnitTest
             {
                 new UserTask { Priority = TaskPriority.High.ToString() },
                 new UserTask { Priority = TaskPriority.Medium.ToString() },
-                new UserTask { Priority = TaskPriority.Low.ToString() }
+                new UserTask { Priority = TaskPriority.Low.ToString() },
+                new UserTask { Priority = TaskPriority.Undefined.ToString() }
             };
         }
 
-        [Test]
-        public void FilterHighPriority()
+        [TestCase(TaskPriority.High, FilterType.HighPriority)]
+        [TestCase(TaskPriority.Medium, FilterType.MediumPriority)]
+        [TestCase(TaskPriority.Low, FilterType.LowPriority)]
+        public void FilterPriority(TaskPriority priority, FilterType filterType)
         {
             var filter = new TasksFilter();
             var usertasks = GenerateUserTaskList();
 
-            var result = filter.Filter(usertasks.AsQueryable(), FilterType.HighPriority);
+            var result = filter.Filter(usertasks.AsQueryable(), filterType);
 
-            Assert.IsTrue(result.All(t => t.Priority == TaskPriority.High.ToString()));
+            Assert.IsTrue(result.All(t => t.Priority == priority.ToString()));
+        }
+
+        [Test]
+        public void FilterPriorityAll()
+        {
+            var filter = new TasksFilter();
+            var usertasks = GenerateUserTaskList();
+
+            var result = filter.Filter(usertasks.AsQueryable(), FilterType.All);
+
+            Assert.IsTrue(result.Count() == usertasks.Count());
+        }
+
+        [Test]
+        public void FilterTypeUndefined()
+        {
+            var filter = new TasksFilter();
+            var usertasks = GenerateUserTaskList();
+
+            Assert.Throws<ArgumentException>(() => 
+                filter.Filter(usertasks.AsQueryable(), FilterType.Undefined)
+            );
+        }
+
+        [Test]
+        public void FilterQueryNull()
+        {
+            var filter = new TasksFilter();
+            var usertasks = GenerateUserTaskList();
+
+            Assert.Throws<ArgumentNullException>(() =>
+                filter.Filter(null, FilterType.All)
+            );
         }
     }
 }
