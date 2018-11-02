de8051b09e1300b70ef30f1b3899d382ee8dbb8f
GKenar <kenar.gf@gmail.com>
2018-11-02 20:43:33 +0200

    Убраны лишние проверки в ConvertToViewPriority

--- TaskManager/PresenterComponents/PriorityConverter.cs
+++ TaskManager/PresenterComponents/PriorityConverter.cs
@@ -21,17 +21,10 @@ namespace TaskManager.PresenterComponents
             if (!success)
                 throw new ArgumentException("The string is not reducible to enum.");
 
-            switch (modelPriority)
-            {
-                case TaskPriority.High:
-                    return TaskPriority.High;
-                case TaskPriority.Medium:
-                    return TaskPriority.Medium;
-                case TaskPriority.Low:
-                    return TaskPriority.Low;
-                default:
-                    throw new ArgumentException("Unknow priority");
-            }
+            if(modelPriority == TaskPriority.Undefined)
+                throw new ArgumentException("Unknow priority");
+
+            return modelPriority;
         }
     }
 }
