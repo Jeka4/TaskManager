25cc8f57ae9ede622a19a433b5ffbc765137d102
GKenar <kenar.gf@gmail.com>
2018-11-17 15:59:35 +0200

    Добавлен класс UserTaskValidator

---
+++ TaskManagerModel/Validators/UserTaskValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TaskManagerModel.Validators
+{
+    public class UserTaskValidator : AbstractValidator<UserTask>
+    {
+        public UserTaskValidator() //Можно добавить сообщения при ошибках валидации
+        {
+            RuleFor(task => task.Id).GreaterThan(0).WithMessage("ID should be greater than 0");
+            RuleFor(task => task.Name).NotNull().NotEmpty();
+            RuleFor(task => task.Description).NotNull().NotEmpty();
+            //RuleFor(task => task.Priority).NotNull().NotEmpty();
+        }
+    }
+}
