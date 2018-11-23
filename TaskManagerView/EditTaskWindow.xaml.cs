3c3b5ecf93b34aa332a59fd532a8ef1359d83ecc
GKenar <kenar.gf@gmail.com>
2018-11-23 15:24:13 +0200

    Простая система отлова ошибки перед добавлением\изменением задачи

--- TaskManagerView/EditTaskWindow.xaml.cs
+++ TaskManagerView/EditTaskWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using TaskManagerView.Components;
 
 namespace TaskManagerView
@@ -17,6 +18,16 @@ namespace TaskManagerView
 
         private void buttonAdd2_Click(object sender, RoutedEventArgs e)
         {
+            if (Validation.GetHasError(textboxName) ||
+                Validation.GetHasError(textboxDescription) ||
+                Validation.GetHasError(comboboxPriority) ||
+                Validation.GetHasError(datapickerTaskDate) ||
+                Validation.GetHasError(datapickerNotifyDate))
+            {
+                MessageBox.Show("Error!");
+                return;
+            }
+
             DialogResult = true;
         }
     }
