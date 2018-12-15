2010bbe232f54cb6d835ae33707c69dc0be011b2
GKenar <kenar.gf@gmail.com>
2018-12-15 18:23:09 +0200

    Добавлено оповещение

--- TaskManagerNotifier/Notifier.cs
+++ TaskManagerNotifier/Notifier.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using TaskManagerModel;
 
@@ -13,6 +15,12 @@ namespace TaskManagerNotifier
 
         private readonly NotifyIcon _notifyIcon;
 
+        private DateTime appCurrentTime;
+
+        private const int TICK_INTERVAL = 1000;
+
+        private const int BALLOONTIP_SHOW_TIME = 3000;
+
         public Notifier(IDataModel dataModel)
         {
             _dataModel = dataModel;
@@ -20,28 +28,49 @@ namespace TaskManagerNotifier
             _notifyIcon = new NotifyIcon();
             _notifyIcon.Text = "TaskManager";
             _notifyIcon.Icon = new System.Drawing.Icon("appicon.ico");
-            //_notifyIcon.ContextMenu = contextMenu;
             _notifyIcon.Visible = true;
             _notifyIcon.DoubleClick += NotifyIconOnDoubleClick;
+
+            _notifyIcon.BalloonTipTitle = "TaskManager";
+            _notifyIcon.BalloonTipText = "Есть задачи, назначенные на сегодня!";
+
+            Task.Run((Action)Tick);
         }
 
         public void OnTasksDataUpdated()
         {
-
+            //CheckForNotifyDates();
         }
 
-        private void LoadNotifyDates()
+        private void CheckForNotifyDates()
         {
             try
             {
-                var notifyDates = _dataModel.GetAllTasks()
-                                            .Where(t => t.TaskDate == DateTime.Today)
-                                            .Select(t => t.NotifyDate);
+                var notifyDates = _dataModel.GetTaskNotifyDates(appCurrentTime);
+
+                if(notifyDates.Count() == 0)
+                    return;
+
+                _notifyIcon.ShowBalloonTip(BALLOONTIP_SHOW_TIME);
             }
             catch (Exception)
             {
 
                 throw;
+            }
+        }
+
+        private void Tick() //Завершать?
+        {
+            while (true)
+            {
+                if (appCurrentTime != DateTime.Today)
+                {
+                    appCurrentTime = DateTime.Today;
+                    CheckForNotifyDates();
+                }
+
+                Thread.Sleep(TICK_INTERVAL);
             }
         }
 
